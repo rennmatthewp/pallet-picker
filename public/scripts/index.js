e371f056c21e0e9b78dc2e4a88c75ff622bd9206
Matthew Renn <rennmatthewp@gmail.com>
2018-03-19 12:55:45 -0600

    generates color palette on page load

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,4 +1,37 @@
-document.querySelector('button').addEventListener('click', (e) => {
-  e.preventDefault();
-  alert('Button clicked!');
+const blocks = [
+  { color: '#FFF', locked: false },
+  { color: '#FFF', locked: false },
+  { color: '#FFF', locked: false },
+  { color: '#FFF', locked: false },
+  { color: '#FFF', locked: false }
+];
+
+const generateHexCode = () => {
+  const chars = '0123456789ABCDEF';
+  let hexCode = '#';
+  for (let i = 0; i < 6; i++) {
+    hexCode += chars[Math.floor(Math.random() * 16)];
+  }
+  return hexCode;
+};
+
+const generatePalette = () => {
+  blocks.forEach((block, index) => {
+    if (block.locked === false) {
+      const hexCode = generateHexCode();
+      block.color = hexCode;
+      $(`.block-${index}`).css('background-color', hexCode);
+      $(`.block-${index}-hex`).text(hexCode);
+    }
+  });
+};
+
+const lockColor = event => {
+  console.log(event.target)
+};
+
+$('.color-block').on('click', lockColor);
+
+$('document').ready(() => {
+  generatePalette();
 });
