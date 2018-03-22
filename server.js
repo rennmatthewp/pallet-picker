53df79ba084df5d0498faf6a00811446ee94f624
Matthew Renn <rennmatthewp@gmail.com>
2018-03-22 11:06:15 -0600

    switches order in exports.down in order to drop tables prettify server.js :

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const apiRoutes = require('./routes/api')
+const apiRoutes = require('./routes/api');
 
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
