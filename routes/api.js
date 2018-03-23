021672e7892987161cbc329c8ba2dde6665dc1de
Matthew Renn <rennmatthewp@gmail.com>
2018-03-23 11:57:09 -0600

    adds endpoint for posting projects adds endpoint for getting palettes adds endpoint for posting palettes

--- routes/api.js
+++ routes/api.js
@@ -11,21 +11,58 @@ router.get('/projects', (request, response) => {
     .then(projects => {
       response.status(200).json(projects);
     })
-    .catch(err => {
-      console.log(err);
+    .catch(error => {
+      console.log(error);
     });
 });
 
 router.post('/projects', (request, response) => {
+  const project = request.body;
+  for (let requiredParam of ['name']) {
+    if (!project[requiredParam]) {
+      return response.status(422).send({
+        error: `Expected format: { name: <String> }. Missing "${requiredParameter}" property.`
+      });
+    }
+  }
+
   database('projects')
-    .insert(request.body , 'id')
+    .insert(project, 'id')
     .then(project => {
       response.status(201).json({ id: project[0] });
     })
-    .catch(err => {
-      console.log('error', err);
-      response.status(500).json(err);
+    .catch(error => {
+      response.status(500).json(error);
     });
+});
+
+router.get('/palettes', (request, response) => {
+  database
+    .select()
+    .table('palettes')
+    .then(palettes => {
+      response.status(200).json(palettes);
+    })
+    .catch(error => console.log(error));
+});
+
+router.post('/palettes', (request, response) => {
+  const palette = request.body;
+  console.log('router.post:', palette);
+  for (let requiredParameter of ['name', 'palette', 'project_id']) {
+    if (!palette[requiredParameter]) {
+      return response.status(422).send({
+        error: `Expected format: { name: <String>, palette: <String>, project_id: <Integer> }. Missing "${requiredParameter}" property.`
+      });
+    }
+  }
+
+  database('palettes')
+    .insert(palette, 'id')
+    .then(palette => {
+      response.status(201).json({ id: palette[0] });
+    })
+    .catch(error => response.status(500).json(error));
 });
 
 module.exports = router;
